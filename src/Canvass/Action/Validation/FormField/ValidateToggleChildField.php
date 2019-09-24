3dc3f07113f05c31d711749e7bd362f92d805e23
Michael Kelly <puritandesigns@gmail.com>
2019-09-24 15:52:46 -0400

    Reduce validation keys for toggle child field

--- src/Canvass/Action/Validation/FormField/ValidateToggleChildField.php
+++ src/Canvass/Action/Validation/FormField/ValidateToggleChildField.php
@@ -7,11 +7,8 @@ abstract class ValidateToggleChildField extends AbstractValidateFieldAction
     public static function getValidationKeysWithRequiredValue(): array
     {
         return [
-            'name' => false,
             'label' => true,
             'identifier' => true,
-            'type' => false,
-            'options' => false,
             'classes' => false,
             'value' => true,
             'help_text' => false,
