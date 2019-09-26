9054c680f0efc3e3a5aa746328a3998995ce3402
Michael Kelly <puritandesigns@gmail.com>
2019-09-26 17:07:47 -0400

    Check for out of bounds sort values in Move action

--- src/Canvass/Action/FormField/MoveField.php
+++ src/Canvass/Action/FormField/MoveField.php
@@ -2,18 +2,34 @@
 
 namespace Canvass\Action\FormField;
 
+use Canvass\Exception\InvalidSortException;
+
 final class MoveField extends AbstractFieldAction
 {
     public const UP = -1;
     public const DOWN = 1;
 
-    public function run(int $direction)
+    public function run(int $direction, $parent_id = 0)
     {
         $old_sort = (int) $this->field->getAttribute('sort');
 
         $new_sort = $old_sort + $direction;
 
-        $other_field = $this->form->findFieldWithSortOf($new_sort);
+        if ($new_sort < 1) {
+            throw new InvalidSortException(
+                'A field cannot have a sort less than one.'
+            );
+        }
+
+        $fields = $this->form->findFields($parent_id);
+
+        if ($new_sort > count($fields->toArray())) {
+            throw new InvalidSortException(
+                'A field cannot have a sort more than the number of fields.'
+            );
+        }
+
+        $other_field = $this->form->findFieldWithSortOf($new_sort, $parent_id);
 
         $this->field->setAttribute('sort', $new_sort);
 
