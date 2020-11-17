3810b43b3275e4ee98f180d8f39f5456c39fb10f
Michael Kelly <puritandesigns@gmail.com>
2020-11-17 17:29:54 -0500

    Fix form validation array

--- src/Canvass/Action/Validation/ValidateFormData.php
+++ src/Canvass/Action/Validation/ValidateFormData.php
@@ -4,36 +4,44 @@ namespace Canvass\Action\Validation;
 
 final class ValidateFormData extends AbstractValidateDataAction
 {
+    public function validate($data): bool
+    {
+        return parent::validate($data);
+    }
+
     public function getValidationRules(): array
     {
         return [
-            'name' => [
+            'name' => ['rules' => [
                 'required' => true,
                 'max_length' => 160,
-            ],
-            'introduction' => [
+            ]],
+            'introduction' => ['rules' => [
                 'required' => false,
-            ],
-            'redirect_url' => [
+            ]],
+            'redirect_url' => ['rules' => [
                 'required' => false,
                 'max_length' => 160,
-            ],
-            'identifier' => [
+            ]],
+            'confirmation_message' => ['rules' => [
+                'required' => false,
+            ]],
+            'identifier' => ['rules' => [
                 'required' => true,
                 'max_length' => 160,
-            ],
-            'classes' => [
+            ]],
+            'classes' => ['rules' => [
                 'required' => false,
                 'max_length' => 160,
-            ],
-            'button_text' => [
+            ]],
+            'button_text' => ['rules' => [
                 'required' => true,
                 'max_length' => 160,
-            ],
-            'button_classes' => [
+            ]],
+            'button_classes' => ['rules' => [
                 'required' => false,
                 'max_length' => 160,
-            ],
+            ]],
         ];
     }
 }
